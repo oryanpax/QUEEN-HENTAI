13989157d903862336f4333c370dde1ed130ed8a
BAWASEER <128925277+bawaseerxd@users.noreply.github.com>
2023-07-14 11:40:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Queen Hentai', {
+say('Bakchod Bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`'${name}' By Dinuwa Official²⁰²³._`, {
+say(`'${name}' By Misa Amane²⁰²³._`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
